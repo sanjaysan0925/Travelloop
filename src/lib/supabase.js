c5eae529f2c685dade6050c9b189eee69199593f
Sanjay <24becse169@kahedu.edu.in>
2026-05-10 13:40:20 +0530

    feat: Sanjay - update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 // src/lib/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mutrdkubqtknkxmpmped.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11dHJka3VicXRrbmt4bXBtcGVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNzE4NzIsImV4cCI6MjA5Mzk0Nzg3Mn0.3rB_90m9V435vMVnd6Q5OdcDyuGZsEfKGTNIRJTUOqE'
+const supabaseUrl = 'https://xasrnkavsxxfflwgmoin.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhhc3Jua2F2c3h4ZmZsd2dtb2luIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzODEzNDEsImV4cCI6MjA5Mzk1NzM0MX0.nhYRl06wsr7Y0aFrCNp2uE3SUB4oM_l3zfUCC7O7eU0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
